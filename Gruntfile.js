7305bd8377a8ca7a20fe47e7774bc16ae3c8f434
Zeno Rocha <zeno.rocha@liferay.com>
2014-02-05 16:34:39 -0300

    Keeping it simple - Remove JSHint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,15 @@
 module.exports = function(grunt) {
 
-  "use strict";
+	grunt.initConfig({
+		connect: {
+			demo: {
+				options: {
+					keepalive: true
+				}
+			}
+		}
+	});
 
-  grunt.initConfig({
-    connect: {
-      demo: {
-        options:{
-          port: 8000,
-          base: '',
-          keepalive: true
-        }
-      }
-    },
-    jshint:{
-      all: [
-        'Gruntfile.js',
-        'src/*.js'
-      ]
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 
 };
