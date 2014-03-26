157eb939aaa16f66da7b6f7d65c7dce3376c6ed9
Zeno Rocha <zeno.rocha@liferay.com>
2014-03-26 19:42:52 -0300

    Open URL in the browser after creating server

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
         connect: {
             demo: {
                 options: {
+                    open: true,
                     keepalive: true
                 }
             }
